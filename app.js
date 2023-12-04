b641ff7a64cdfa180c74e17933438071af8524be
taoyuan <yuan.ta@northeastern.edu>
2023-12-04 05:54:21 -0500

    a6 Mongo fa23

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import Hello from './hello.js';
+import Lab5 from './Lab5.js';
+import CourseRoutes from "./courses/routes.js";
+import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+var corsOptions = {
+
+    credentials: true,
+    origin: '*'
+    
+  }
+const app = express()
+app.use(
+    cors(corsOptions)
+);  
+app.use(express.json());
+
+Hello(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app)
+app.listen(process.env.PORT || 4000);
+
